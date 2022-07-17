734d4977333743438c709b284e250c278977ac54
Priyanshu Pattanaik <hi_priyanshu@outlook.com>
2022-07-17 16:31:07 +0530

    Update 5_Who'sBuyingLunch.js

--- 5_Who'sBuyingLunch.js
+++ 5_Who'sBuyingLunch.js
@@ -1,7 +1,7 @@
 /*
 PROBLEM DESCRIPTION:
 
-
+Write a function which will select a random name from a list of names. The person selected will have to pay for everybody's food bill.
 */
 
 // SOLUTION
